21d1a34be9d9edec4983a75768680131db2b7222
NHA0503 <99701304+NHA0503@users.noreply.github.com>
2023-05-14 15:54:13 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'cute';
+const giftUrl = 'https://nha0503.github.io/HOanh/';
+const eventName = 'Hằng';
+const titleCard = 'Gửi Hằng đáng yêu';
+const contentCard = 'Hằng biết không, có những lần HAnh chỉ muốn ôm Hằng thật lâu sau khi nghe Hằng kể về những gì mà Hằng đã trải qua, HAnh ước gì mình gặp Hằng sớm hơn. Nhưng mà, quan trọng là giờ HAnh ở đây rồi, quá khứ của Hằng cứ mặc nó ngủ yên, không nói chuyện ngày mai. Điều duy nhất HAnh quan tâm bây giờ là Hằng. Vui hay buồn gì đều có nhau là đủ rồi. Nếu cảm thấy tương lai đáng sợ, nhắm mắt lại HAnh sẽ dẫn Hằng đi';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'hot-girl.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
